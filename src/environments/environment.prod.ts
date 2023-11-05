247ad6da81b32d232d9c3317aaa80b6002f04880
Jad Chahed <jad.chahed@bashroom.com>
2023-11-05 18:20:35 +0100

    feat: gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
 };
+
+export const rapidApiKeys = {
+  XRapidAPIKey: '2QMXSehDLSmshDmRQcKUIAiQjIZAp1UvKUrjsnewgqSP6F5oBX',
+  XRapidAPIHost: 'free-nba.p.rapidapi.com',
+};
+
+export const rapidBaseUrl = 'https://free-nba.p.rapidapi.com';
+export const logoBaseUrl = 'https://interstate21.com/nba-logos';
